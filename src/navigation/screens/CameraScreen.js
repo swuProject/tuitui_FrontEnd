58e79af4393cf0dacf81ce93c401526cda957507
Namgyeon <getam101@naver.com>
2024-06-03 16:16:50 +0900

    최신화

--- src/navigation/screens/CameraScreen.js
+++ src/navigation/screens/CameraScreen.js
@@ -1,10 +1,41 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function CameraScreen() {
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+
+function CameraScreen() {
+  // const cameraPermission = Camera.getCameraPermissionStatus()
+  // const microphonePermission = Camera.getMicrophonePermissionStatus()
+
+  // const newCameraPermission = await Camera.requestCameraPermission()
+  // const newMicrophonePermission = await Camera.requestMicrophonePermission()
+
+  // const device = useCameraDevice('back')
+  // const { hasPermission } = useCameraPermission()
+
+  // if (!hasPermission) return <PermissionsPage />
+  // if (device == null) return <NoCameraDeviceError />
+  // return (
+  //   <Camera
+  //     style={StyleSheet.absoluteFill}
+  //     device={device}
+  //     isActive={true}
+  //   />
+  // )
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 26, fontWeight: "bold" }}>Camera Screen</Text>
+    <View>
+      <Text>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default CameraScreen;
